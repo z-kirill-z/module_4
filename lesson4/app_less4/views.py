8236943f9aba14e77ef108888ca91ad93363afa6
zotskirill <zotskirill24@gmail.com>
2023-07-16 16:50:33 +0500

    'less5'

--- lesson4/app_less4/views.py
+++ lesson4/app_less4/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(requests):
-    return HttpResponse('Успешно!')
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
 
 
