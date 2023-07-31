b4c926c856cfbe2e6507aa9a2d75d00a4c9d0553
zotskirill <zotskirill24@gmail.com>
2023-07-31 16:03:55 +0500

    less7

--- lesson4/app_less4/models.py
+++ lesson4/app_less4/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text="отметьте, если торг возможен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+    class Meta:
+        db_table = '"advertisements"'
