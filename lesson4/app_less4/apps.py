40ce8c1152d84943cc96539f75a86bc58f15239c
zotskirill <zotskirill24@gmail.com>
2023-08-06 16:30:07 +0500

    less8

--- lesson4/app_less4/apps.py
+++ lesson4/app_less4/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class AppLess4Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_less4'
+    verbose_name = 'Объявления'
+
--- lesson4/app_less4/models.py
+++ lesson4/app_less4/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 # Create your models here.
 
 class Advertisement(models.Model):
@@ -9,6 +10,28 @@ class Advertisement(models.Model):
     auction = models.BooleanField('торг', help_text="отметьте, если торг возможен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;"> Сегодня в {} </span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='дата обновления')
+    def created_update(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            created_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: purple; font-weight: bold;"> Сегодня в {} </span>', created_time
+            )
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
     class Meta:
