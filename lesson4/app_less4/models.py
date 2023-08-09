18ca664e283e003b06467f61da16f2f59b95f1bf
zotskirill <zotskirill24@gmail.com>
2023-08-09 14:27:19 +0500

    ne ponyatki

--- lesson4/app_less4/models.py
+++ lesson4/app_less4/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # Create your models here.
 
 class Advertisement(models.Model):
@@ -10,6 +13,7 @@ class Advertisement(models.Model):
     auction = models.BooleanField('торг', help_text="отметьте, если торг возможен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
 
     @admin.display(description='дата создания')
     def created_date(self):
@@ -35,4 +39,4 @@ class Advertisement(models.Model):
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
     class Meta:
-        db_table = '"advertisements"'
+        db_table = 'advertisements'
--- lesson4/app_less4/views.py
+++ lesson4/app_less4/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Advertisement
 
 def index(request):
-    return render(request, 'index.html')
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
 
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+
+
 
 
