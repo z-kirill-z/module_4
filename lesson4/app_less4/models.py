57220d8234819cc51fb17d7d03f508d7b82448d9
romkap0 <roman.kradyk@gmail.com>
2023-08-17 11:50:54 +0300

    fix tables

--- lesson4/app_less4/models.py
+++ lesson4/app_less4/models.py
@@ -4,6 +4,8 @@ from django.utils.html import format_html
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
 # Create your models here.
 
 class Advertisement(models.Model):
@@ -13,7 +15,7 @@ class Advertisement(models.Model):
     auction = models.BooleanField('торг', help_text="отметьте, если торг возможен")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE, default=1)
 
     @admin.display(description='дата создания')
     def created_date(self):
@@ -35,8 +37,8 @@ class Advertisement(models.Model):
             )
         return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
 
-
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
     class Meta:
         db_table = 'advertisements'
