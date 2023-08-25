4f0b44dcb97018e8c824e3e83556a95e6e73c67f
zotskirill <zotskirill24@gmail.com>
2023-08-25 11:32:39 +0500

    less10

--- lesson4/app_less4/urls.py
+++ lesson4/app_less4/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement_post
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='main.page'),
     path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='adv-post')
 ]
 
 if settings.DEBUG:
--- lesson4/app_less4/views.py
+++ lesson4/app_less4/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
+
 from .models import Advertisement
+from .forms import AdvertisementForm
 
 def index(request):
     advertisements = Advertisement.objects.all()
@@ -10,6 +13,17 @@ def index(request):
 def top_sellers(request):
     return render(request, 'top-sellers.html')
 
-
-
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main.page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
 
